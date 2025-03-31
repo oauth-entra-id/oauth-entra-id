d132e4d64db69940dddc1e400d3c39f04cd88a06
WolfieLeader <koren2318@gmail.com>
2025-03-31 15:42:11 +0300

    Vite Projects have their own env files

--- frontends/react18/src/env.ts
+++ frontends/react18/src/env.ts
@@ -1,12 +1,12 @@
 import { z } from 'zod';
 
-const zStr = z.string().trim();
+const zUrl = z.string().trim().url().min(1);
 
 const zEnv = z.object({
-  VITE_EXPRESS_URL: zStr.url().default('https://localhost:3001'),
-  VITE_FASTIFY_URL: zStr.url().default('https://localhost:3002'),
-  VITE_NESTJS_URL: zStr.url().default('https://localhost:3003'),
-  VITE_HONOJS_URL: zStr.url().default('https://localhost:3004'),
+  VITE_EXPRESS_URL: zUrl,
+  VITE_NESTJS_URL: zUrl,
+  VITE_FASTIFY_URL: zUrl,
+  VITE_HONOJS_URL: zUrl,
 });
 
 const parsedEnv = zEnv.safeParse(import.meta.env);
@@ -16,8 +16,8 @@ if (!parsedEnv.success) {
 }
 
 export const {
-  VITE_HONOJS_URL: HONOJS_SERVER,
   VITE_EXPRESS_URL: EXPRESS_SERVER,
-  VITE_FASTIFY_URL: FASTIFY_SERVER,
   VITE_NESTJS_URL: NESTJS_SERVER,
+  VITE_FASTIFY_URL: FASTIFY_SERVER,
+  VITE_HONOJS_URL: HONOJS_SERVER,
 } = parsedEnv.data;
